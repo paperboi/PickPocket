d51b0f0b167fd67a6a12065964e54475d288936e
paperboi <jeffreysamjacob@gmail.com>
2020-08-18 18:57:49 +0530

    fixes #3 #4 #5

--- fetchTitleFromURL.py
+++ fetchTitleFromURL.py
@@ -1,17 +1,11 @@
 import requests
 from time import sleep
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
 from bs4 import BeautifulSoup
 
 def fetchTitleFromURL(url):
     session = requests.Session()
-    retry = Retry(connect=3, backoff_factor=0.5)
-    adapter = HTTPAdapter(max_retries=retry)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
     r = session.get(url)
-    # sleep(1)
+    sleep(1)
     soup = BeautifulSoup(r.content, 'lxml')
     title = soup.select_one('title').text
     return(title)
